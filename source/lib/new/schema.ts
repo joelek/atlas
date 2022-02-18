ef505c38feee358653a476aa7bd947f757e059d6
Joel Ek <contact@joelek.se>
2022-02-18 16:40:58 +0100

    Add all schemas.

--- source/lib/new/schema.ts
+++ source/lib/new/schema.ts
@@ -1,99 +1,60 @@
-export class SchemaManager {
-	constructor() {
+import * as bedrock from "@joelek/bedrock";
 
-	}
-};
+export const BinaryFieldSchema = bedrock.codecs.Object.of({
+	type: bedrock.codecs.StringLiteral.of("Binary"),
+	defaultValue: bedrock.codecs.Binary
+});
 
-/*
-	static saveSchema(): void {
-		let storeBids = {} as DatabaseSchema["storeBids"];
-		for (let key in this.storeManagers) {
-			storeBids[key] = this.storeManagers[key].getBid();
-		}
-		let links = {} as DatabaseSchema["links"];
-		for (let key in this.linkManagers) {
-			links[key] = this.linkManagers[key].getBid();
-		}
-		let schema: DatabaseSchema = {
-			storeBids,
-			links
-		};
-		let buffer = DatabaseSchema.encode(schema);
-		this.blockHandler.resizeBlock(this.bid, buffer.length);
-		this.blockHandler.writeBlock(this.bid, buffer);
-	}
- */
-	static migrate<A extends Stores<A>, B extends Links<B>>(oldDatabase: TransactionManager<any, any>, options: {
-		stores: A,
-		links: B
-	}): TransactionManager<A, B> {
-		let migratedStores = [] as Array<keyof A>;
-		for (let key in options.stores) {
-			if (oldDatabase.storeManagers[key] == null) {
+export type BinaryFieldSchema = ReturnType<typeof BinaryFieldSchema["decode"]>;
 
-			}
-		}
-		for (let key in oldDatabase.storeManagers) {
-			if (options.stores[key as string as keyof A] == null) {
-				// remove store
-			}
-		}
-	}
+export const BooleanFieldSchema = bedrock.codecs.Object.of({
+	type: bedrock.codecs.StringLiteral.of("Boolean"),
+	defaultValue: bedrock.codecs.Boolean
+});
 
-	// TODO: Handle schema outside.
-	static construct<A extends Stores<A>, B extends Links<B>>(blockHandler: BlockHandler, bid: number | null, file: File, options?: {
-		stores: A,
-		links: B
-	}): TransactionManager<A, B> {
-		if (bid == null) {
-			if (options == null) {
-				return TransactionManager.construct<any, any>(blockHandler, null, file, {
-					stores: {},
-					links: {}
-				});
-			} else {
-				let storeManagers = {} as StoreManagers<A>;
-				for (let key in options.stores) {
-					storeManagers[key] = options.stores[key].createManager(blockHandler, null) as any;
-				}
-				let linkManagers = {} as LinkManagers<B>;
-				for (let key in options.links) {
-					linkManagers[key] = options.links[key].createManager(blockHandler, null) as any;
-				}
-				bid = blockHandler.createBlock(64);
-				let manager = new TransactionManager(blockHandler, bid, file, storeManagers, linkManagers);
-				manager.saveSchema();
-				return manager;
-			}
-		} else {
-			if (options == null) {
-				let schema = DatabaseSchema.decode(blockHandler.readBlock(bid));
-				let storeManagers = {} as StoreManagers<A>;
-				for (let key in schema.storeBids) {
-					storeManagers[key as keyof A] = StoreManager.construct(blockHandler, schema.storeBids[key]) as any;
-				}
-				let linkManagers = {} as LinkManagers<B>;
-				for (let key in schema.linkBids) {
-					linkManagers[key as keyof A] = LinkManager.construct(blockHandler, schema.linkBids[key]) as any;
-				}
-				let manager = new TransactionManager(blockHandler, bid, file, storeManagers, linkManagers);
-				return manager;
-			} else {
-				return TransactionManager.migrate(TransactionManager.construct(blockHandler, bid, file), options);
-			}
-		}
-	}
-};
+export type BooleanFieldSchema = ReturnType<typeof BooleanFieldSchema["decode"]>;
 
+export const StringFieldSchema = bedrock.codecs.Object.of({
+	type: bedrock.codecs.StringLiteral.of("String"),
+	defaultValue: bedrock.codecs.String
+});
 
+export type StringFieldSchema = ReturnType<typeof StringFieldSchema["decode"]>;
 
+export const NullableStringFieldSchema = bedrock.codecs.Object.of({
+	type: bedrock.codecs.StringLiteral.of("NullableString"),
+	defaultValue: bedrock.codecs.Union.of(
+		bedrock.codecs.String,
+		bedrock.codecs.Null
+	)
+});
 
+export type NullableStringFieldSchema = ReturnType<typeof NullableStringFieldSchema["decode"]>;
 
+export const FieldSchema = bedrock.codecs.Union.of(
+	BinaryFieldSchema,
+	BooleanFieldSchema,
+	StringFieldSchema,
+	NullableStringFieldSchema
+);
 
+export type FieldSchema = ReturnType<typeof FieldSchema["decode"]>;
 
+export const IndexSchema = bedrock.codecs.Object.of({
+	keys: bedrock.codecs.Array.of(bedrock.codecs.String),
+	bid: bedrock.codecs.Integer
+});
 
+export type IndexSchema = ReturnType<typeof IndexSchema["decode"]>;
 
+export const StoreSchema = bedrock.codecs.Object.of({
+	fields: bedrock.codecs.Record.of(FieldSchema),
+	keys: bedrock.codecs.Array.of(bedrock.codecs.String),
+	indices: bedrock.codecs.Array.of(IndexSchema),
+	storageBid: bedrock.codecs.Integer
+});
 
+export type StoreSchema = ReturnType<typeof StoreSchema["decode"]>;
 
 export const LinkSchema = bedrock.codecs.Object.of({
 	parent: bedrock.codecs.String,
@@ -104,7 +65,7 @@ export const LinkSchema = bedrock.codecs.Object.of({
 export type LinkSchema = ReturnType<typeof LinkSchema["decode"]>;
 
 export const DatabaseSchema = bedrock.codecs.Object.of({
-	storeBids: bedrock.codecs.Record.of(bedrock.codecs.Integer),
+	stores: bedrock.codecs.Record.of(StoreSchema),
 	links: bedrock.codecs.Record.of(LinkSchema)
 });
 
