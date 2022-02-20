e9f46a5fadc7023d895c1cb7638a98da27333ed0
Joel Ek <contact@joelek.se>
2022-02-20 13:19:13 +0100

    Continue work on ConsistencyManager.

--- source/lib/new/consistency.ts
+++ source/lib/new/consistency.ts
@@ -38,102 +38,126 @@ import { LinkManager, LinkManagers, Links, WritableLink, WritableLinks } from ".
 import { Record, Keys, KeysRecordMap } from "./records";
 import { StoreManager, StoreManagers, Stores, WritableStore, WritableStores } from "./store";
 
-export class ConsistentWritableStore<A extends Record, B extends Keys<A>> implements WritableStore<A, B> {
+export class OverridableWritableStore<A extends Record, B extends Keys<A>> implements WritableStore<A, B> {
 	private storeManager: StoreManager<A, B>;
-	private linksWhereStoreIsParent: Set<LinkManager<any, any, any, any, any>>;
-	private linksWhereStoreIsChild: Set<LinkManager<any, any, any, any, any>>;
+	private overrides: Partial<WritableStore<A, B>>;
 
-	constructor(storeManager: StoreManager<A, B>, linksWhereStoreIsParent: Set<LinkManager<any, any, any, any, any>>, linksWhereStoreIsChild: Set<LinkManager<any, any, any, any, any>>) {
+	constructor(storeManager: StoreManager<A, B>, overrides: Partial<WritableStore<A, B>>) {
 		this.storeManager = storeManager;
-		this.linksWhereStoreIsParent = linksWhereStoreIsParent;
-		this.linksWhereStoreIsChild = linksWhereStoreIsChild;
+		this.overrides = overrides;
 	}
 
 	async filter(...parameters: Parameters<WritableStore<A, B>["filter"]>): ReturnType<WritableStore<A, B>["filter"]> {
-		return this.storeManager.filter(...parameters);
+		return this.overrides.filter?.(...parameters) ?? this.storeManager.filter(...parameters);
 	}
 
 	async insert(...parameters: Parameters<WritableStore<A, B>["insert"]>): ReturnType<WritableStore<A, B>["insert"]> {
-		for (let linkManager of this.linksWhereStoreIsChild) {
-			// TODO: Handle valid nulls here?
-			linkManager.lookup(parameters[0]);
-		}
-		return this.storeManager.insert(...parameters);
+		return this.overrides.insert?.(...parameters) ?? this.storeManager.insert(...parameters);
 	}
 
 	async length(...parameters: Parameters<WritableStore<A, B>["length"]>): ReturnType<WritableStore<A, B>["length"]> {
-		return this.storeManager.length(...parameters);
+		return this.overrides.length?.(...parameters) ?? this.storeManager.length(...parameters);
 	}
 
 	async lookup(...parameters: Parameters<WritableStore<A, B>["lookup"]>): ReturnType<WritableStore<A, B>["lookup"]> {
-		return this.storeManager.lookup(...parameters);
+		return this.overrides.lookup?.(...parameters) ?? this.storeManager.lookup(...parameters);
 	}
 
 	async remove(...parameters: Parameters<WritableStore<A, B>["remove"]>): ReturnType<WritableStore<A, B>["remove"]> {
-		// TODO: Add record to deletion queue.
-		for (let linkManager of this.linksWhereStoreIsParent) {
-			let entries = linkManager.filter(parameters[0]);
-			// TODO: Add entries to deletion queue.
-		}
-		return this.storeManager.remove(...parameters);
+		return this.overrides.remove?.(...parameters) ?? this.storeManager.remove(...parameters);
 	}
 
 	async update(...parameters: Parameters<WritableStore<A, B>["update"]>): ReturnType<WritableStore<A, B>["update"]> {
-		return this.storeManager.update(...parameters);
+		return this.overrides.update?.(...parameters) ?? this.storeManager.update(...parameters);
 	}
 };
 
-export class ConsistentWritableLink<A extends Record, B extends Keys<A>, C extends Record, D extends Keys<C>, E extends KeysRecordMap<A, B, C>> implements WritableLink<A, B, C, D, E> {
+export class OverridableWritableLink<A extends Record, B extends Keys<A>, C extends Record, D extends Keys<C>, E extends KeysRecordMap<A, B, C>> implements WritableLink<A, B, C, D, E> {
 	private linkManager: LinkManager<A, B, C, D, E>;
+	private overrides: Partial<WritableLink<A, B, C, D, E>>;
 
-	constructor(linkManager: LinkManager<A, B, C, D, E>) {
+	constructor(linkManager: LinkManager<A, B, C, D, E>, overrides: Partial<WritableLink<A, B, C, D, E>>) {
 		this.linkManager = linkManager;
+		this.overrides = overrides;
 	}
 
 	async filter(...parameters: Parameters<WritableLink<A, B, C, D, E>["filter"]>): ReturnType<WritableLink<A, B, C, D, E>["filter"]> {
-		return this.linkManager.filter(...parameters);
+		return this.overrides.filter?.(...parameters) ?? this.linkManager.filter(...parameters);
 	}
 
 	async lookup(...parameters: Parameters<WritableLink<A, B, C, D, E>["lookup"]>): ReturnType<WritableLink<A, B, C, D, E>["lookup"]> {
-		return this.linkManager.lookup(...parameters);
+		return this.overrides.lookup?.(...parameters) ?? this.linkManager.lookup(...parameters);
 	}
 };
 
 export class ConsistencyManager<A, B> {
 	private storeManagers: StoreManagers<Stores<A>>;
 	private linkManagers: LinkManagers<Links<B>>;
+	private linksWhereStoreIsParent: Map<StoreManager<any, any>, Set<LinkManager<any, any, any, any, any>>>;
+	private linksWhereStoreIsChild: Map<StoreManager<any, any>, Set<LinkManager<any, any, any, any, any>>>;
 
 	private getLinksWhereStoreIsParent(storeManager: StoreManager<any, any>): Set<LinkManager<any, any, any, any, any>> {
-		let linksWhereStoreIsParent = new Set<LinkManager<any, any, any, any, any>>();
-		for (let key in this.linkManagers) {
-			if (storeManager === this.linkManagers[key].getParent()) {
-				linksWhereStoreIsParent.add(this.linkManagers[key]);
-			}
+		let set = this.linksWhereStoreIsParent.get(storeManager);
+		if (set == null) {
+			throw `Expected link set!`;
 		}
-		return linksWhereStoreIsParent;
+		return set;
 	}
 
 	private getLinksWhereStoreIsChild(storeManager: StoreManager<any, any>): Set<LinkManager<any, any, any, any, any>> {
-		let linksWhereStoreIsChild = new Set<LinkManager<any, any, any, any, any>>();
-		for (let key in this.linkManagers) {
-			if (storeManager === this.linkManagers[key].getChild()) {
-				linksWhereStoreIsChild.add(this.linkManagers[key]);
-			}
+		let set = this.linksWhereStoreIsChild.get(storeManager);
+		if (set == null) {
+			throw `Expected link set!`;
 		}
-		return linksWhereStoreIsChild;
+		return set;
+	}
+
+	private remove(deletionQueues: Map<StoreManager<any, any>, Set<number>>): void {
+
 	}
 
 	constructor(storeManagers: StoreManagers<Stores<A>>, linkManagers: LinkManagers<Links<B>>) {
 		this.storeManagers = storeManagers;
 		this.linkManagers = linkManagers;
+		this.linksWhereStoreIsParent = new Map();
+		this.linksWhereStoreIsChild = new Map();
+		for (let key in storeManagers) {
+			let storeManager = storeManagers[key];
+			let linksWhereStoreIsChild = new Set<LinkManager<any, any, any, any, any>>();
+			let linksWhereStoreIsParent = new Set<LinkManager<any, any, any, any, any>>();
+			for (let key in this.linkManagers) {
+				let linkManager = this.linkManagers[key];
+				if (storeManager === linkManager.getParent()) {
+					linksWhereStoreIsParent.add(linkManager);
+				}
+				if (storeManager === linkManager.getChild()) {
+					linksWhereStoreIsChild.add(linkManager);
+				}
+			}
+			this.linksWhereStoreIsParent.set(storeManager, linksWhereStoreIsParent);
+			this.linksWhereStoreIsChild.set(storeManager, linksWhereStoreIsChild);
+		}
 	}
 
 	createWritableStores(): WritableStores<A> {
 		let writableStores = {} as WritableStores<any>;
 		for (let key in this.storeManagers) {
-			let linksWhereStoreIsParent = this.getLinksWhereStoreIsParent(this.storeManagers[key]);
-			let linksWhereStoreIsChild = this.getLinksWhereStoreIsChild(this.storeManagers[key]);
-			writableStores[key] = new ConsistentWritableStore(this.storeManagers[key], linksWhereStoreIsParent, linksWhereStoreIsChild);
+			let storeManager = this.storeManagers[key];
+			writableStores[key] = new OverridableWritableStore(storeManager, {
+				insert: async (record) => {
+					for (let linkManager of this.getLinksWhereStoreIsChild(storeManager)) {
+						linkManager.lookup(record);
+					}
+					return storeManager.insert(record);
+				},
+				remove: async (record) => {
+					for (let linkManager of this.getLinksWhereStoreIsParent(storeManager)) {
+						let entries = linkManager.filter(record);
+						// TODO: Add entries to deletion queue.
+					}
+					return storeManager.remove(record);
+				}
+			});
 		}
 		return writableStores;
 	}
@@ -141,7 +165,7 @@ export class ConsistencyManager<A, B> {
 	createWritableLinks(): WritableLinks<B> {
 		let writableLinks = {} as WritableLinks<any>;
 		for (let key in this.linkManagers) {
-			writableLinks[key] = new ConsistentWritableLink(this.linkManagers[key]);
+			writableLinks[key] = new OverridableWritableLink(this.linkManagers[key], {});
 		}
 		return writableLinks;
 	}
