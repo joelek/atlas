4821ff6c0ed523a7fd36b8615b9011f5279f6c9f
Joel Ek <contact@joelek.se>
2022-02-21 01:22:23 +0100

    Add some tests for ConsistencyManager.

--- source/lib/new/consistency.test.ts
+++ source/lib/new/consistency.test.ts
@@ -1,5 +1,10 @@
-import { Record, Keys, KeysRecordMap } from "./records";
+import { Record, Keys, KeysRecordMap, StringField, NullableStringField } from "./records";
 import { test } from "../test";
+import { ConsistencyManager } from "./consistency";
+import { BlockHandler } from "./vfs";
+import { VirtualFile } from "./files";
+import { StoreManager } from "./store";
+import { LinkManager } from "./link";
 
 type Store<A extends Record, B extends Keys<A>> = {
 	records: Map<number, A>;
@@ -267,4 +272,209 @@ test(`It should remove orphaned records for linked records in different stores.`
 	});
 	assert.array.equals(Array.from(users.records.keys()).sort(), [0]);
 	assert.array.equals(Array.from(posts.records.keys()).sort(), [1]);
+});
+
+
+
+
+
+
+
+
+
+
+function createUsersPostsAndComments() {
+	let blockHandler = new BlockHandler(new VirtualFile(0));
+	let users = StoreManager.construct(blockHandler, null, {
+		fields: {
+			user_id: new StringField("")
+		},
+		keys: ["user_id"],
+		indices: []
+	});
+	let posts = StoreManager.construct(blockHandler, null, {
+		fields: {
+			post_id: new StringField(""),
+			post_user_id: new StringField("")
+		},
+		keys: ["post_id"],
+		indices: []
+	});
+	let comments = StoreManager.construct(blockHandler, null, {
+		fields: {
+			comment_id: new StringField(""),
+			comment_user_id: new StringField(""),
+			comment_post_id: new StringField("")
+		},
+		keys: ["comment_id"],
+		indices: []
+	});
+	let userPosts = LinkManager.construct(users, posts, {
+		user_id: "post_user_id"
+	});
+	let postComments = LinkManager.construct(posts, comments, {
+		post_id: "comment_post_id"
+	});
+	let userComments = LinkManager.construct(users, comments, {
+		user_id: "comment_user_id"
+	});
+	let consistencyManager = new ConsistencyManager({
+		users,
+		posts,
+		comments
+	}, {
+		userPosts,
+		postComments,
+		userComments
+	});
+	return {
+		storeManagers: {
+			users,
+			posts,
+			comments
+		},
+		writableStores: consistencyManager.createWritableStores()
+	};
+};
+
+function createDirectories() {
+	let blockHandler = new BlockHandler(new VirtualFile(0));
+	let directories = StoreManager.construct(blockHandler, null, {
+		fields: {
+			directory_id: new StringField(""),
+			parent_directory_id: new NullableStringField(null)
+		},
+		keys: ["directory_id"],
+		indices: []
+	});
+	let childDirectories = LinkManager.construct(directories, directories, {
+		directory_id: "parent_directory_id"
+	});
+	let consistencyManager = new ConsistencyManager({
+		directories
+	}, {
+		childDirectories
+	});
+	return {
+		storeManagers: {
+			directories
+		},
+		writableStores: consistencyManager.createWritableStores()
+	};
+};
+
+test(`It should remove orphaned child records for referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createUsersPostsAndComments();
+	storeManagers.users.insert({
+		user_id: "User 0"
+	});
+	storeManagers.posts.insert({
+		post_id: "Post 0",
+		post_user_id: "User 0"
+	});
+	await writableStores.users.remove({
+		user_id: "User 0"
+	});
+	assert.array.equals(Array.from(await writableStores.users.filter()).map((record) => record.record().user_id).sort(), []);
+	assert.array.equals(Array.from(await writableStores.posts.filter()).map((record) => record.record().post_id).sort(), []);
+});
+
+test(`It should remove orphaned grandchild records for referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createUsersPostsAndComments();
+	storeManagers.users.insert({
+		user_id: "User 0"
+	});
+	storeManagers.posts.insert({
+		post_id: "Post 0",
+		post_user_id: "User 0"
+	});
+	storeManagers.comments.insert({
+		comment_id: "Comment 0",
+		comment_post_id: "Post 0",
+		comment_user_id: "User 0"
+	});
+	await writableStores.users.remove({
+		user_id: "User 0"
+	});
+	assert.array.equals(Array.from(await writableStores.users.filter()).map((record) => record.record().user_id).sort(), []);
+	assert.array.equals(Array.from(await writableStores.posts.filter()).map((record) => record.record().post_id).sort(), []);
+	assert.array.equals(Array.from(await writableStores.comments.filter()).map((record) => record.record().comment_id).sort(), []);
+});
+
+test(`It should not remove records with parents for referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createUsersPostsAndComments();
+	storeManagers.users.insert({
+		user_id: "User 0"
+	});
+	storeManagers.posts.insert({
+		post_id: "Post 0",
+		post_user_id: "User 0"
+	});
+	storeManagers.comments.insert({
+		comment_id: "Comment 0",
+		comment_post_id: "Post 0",
+		comment_user_id: "User 0"
+	});
+	await writableStores.posts.remove({
+		post_id: "Post 0"
+	});
+	assert.array.equals(Array.from(await writableStores.users.filter()).map((record) => record.record().user_id).sort(), ["User 0"]);
+	assert.array.equals(Array.from(await writableStores.posts.filter()).map((record) => record.record().post_id).sort(), []);
+	assert.array.equals(Array.from(await writableStores.comments.filter()).map((record) => record.record().comment_id).sort(), []);
+});
+
+test(`It should remove orphaned child records for self-referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createDirectories();
+	storeManagers.directories.insert({
+		directory_id: "Directory 0",
+		parent_directory_id: null
+	});
+	storeManagers.directories.insert({
+		directory_id: "Directory 1",
+		parent_directory_id: "Directory 0"
+	});
+	await writableStores.directories.remove({
+		directory_id: "Directory 0"
+	});
+	assert.array.equals(Array.from(await writableStores.directories.filter()).map((record) => record.record().directory_id).sort(), []);
+});
+
+test(`It should remove orphaned grandchild records for self-referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createDirectories();
+	storeManagers.directories.insert({
+		directory_id: "Directory 0",
+		parent_directory_id: null
+	});
+	storeManagers.directories.insert({
+		directory_id: "Directory 1",
+		parent_directory_id: "Directory 0"
+	});
+	storeManagers.directories.insert({
+		directory_id: "Directory 2",
+		parent_directory_id: "Directory 1"
+	});
+	await writableStores.directories.remove({
+		directory_id: "Directory 0"
+	});
+	assert.array.equals(Array.from(await writableStores.directories.filter()).map((record) => record.record().directory_id).sort(), []);
+});
+
+test(`It should not remove records with parents for self-referencing links.`, async (assert) => {
+	let { storeManagers, writableStores } = createDirectories();
+	storeManagers.directories.insert({
+		directory_id: "Directory 0",
+		parent_directory_id: null
+	});
+	storeManagers.directories.insert({
+		directory_id: "Directory 1",
+		parent_directory_id: "Directory 0"
+	});
+	storeManagers.directories.insert({
+		directory_id: "Directory 2",
+		parent_directory_id: "Directory 1"
+	});
+	await writableStores.directories.remove({
+		directory_id: "Directory 1"
+	});
+	assert.array.equals(Array.from(await writableStores.directories.filter()).map((record) => record.record().directory_id).sort(), ["Directory 0"]);
 });
