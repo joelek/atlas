6c19be477169b197f067aad08d45ffacdcf263de
Joel Ek <contact@joelek.se>
2022-02-22 20:44:18 +0100

    Improve benchmarking code.

--- source/lib/benchmark.ts
+++ source/lib/benchmark.ts
@@ -4,7 +4,8 @@ export function benchmark<A>(subject: () => A, times: number = 1): A {
 	for (let i = 1; i < times; i++) {
 		subject();
 	}
-	let duration = (Number(process.hrtime.bigint() - start) / 1000000 / times);
-	console.log(`${duration.toFixed(6)} ms`, result);
+	let duration_ms = (Number(process.hrtime.bigint() - start) / 1000 / 1000 / times);
+	let ops_per_sec = 1000 / duration_ms;
+	console.log(`${duration_ms.toFixed(6)} ms, ${ops_per_sec.toFixed(0)} ops/s`);
 	return result;
 };
