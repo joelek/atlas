08737d74e40e77b99581a6f0af58e8693fada08b
Joel Ek <contact@joelek.se>
2022-03-04 17:32:31 +0100

    Generate dist artifacts.

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,2 +1,8 @@
 import { Context } from "./contexts";
+import { DecreasingOrder, IncreasingOrder } from "./orders";
+import { EqualityFilter } from "./filters";
+import { Value } from "./records";
 export declare function createContext(): Context;
+export declare function createIncreasingOrder<A extends Value>(): IncreasingOrder<A>;
+export declare function createDecreasingOrder<A extends Value>(): DecreasingOrder<A>;
+export declare function createEqualityFilter<A extends Value>(value: A): EqualityFilter<A>;
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,9 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = void 0;
+exports.createEqualityFilter = exports.createDecreasingOrder = exports.createIncreasingOrder = exports.createContext = void 0;
 const contexts_1 = require("./contexts");
+const orders_1 = require("./orders");
+const filters_1 = require("./filters");
 function createContext() {
     return new contexts_1.Context();
 }
 exports.createContext = createContext;
+;
+function createIncreasingOrder() {
+    return new orders_1.IncreasingOrder();
+}
+exports.createIncreasingOrder = createIncreasingOrder;
+;
+function createDecreasingOrder() {
+    return new orders_1.DecreasingOrder();
+}
+exports.createDecreasingOrder = createDecreasingOrder;
+;
+function createEqualityFilter(value) {
+    return new filters_1.EqualityFilter(value);
+}
+exports.createEqualityFilter = createEqualityFilter;
 ;
