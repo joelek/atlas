6598ce1d6465fe3e1210bdc213c8778a4ae52e91
Joel Ek <contact@joelek.se>
2022-03-04 17:31:56 +0100

    Add orders and filters to index module.

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -1,5 +1,20 @@
 import { Context } from "./contexts";
+import { DecreasingOrder, IncreasingOrder } from "./orders";
+import { EqualityFilter } from "./filters";
+import { Value } from "./records";
 
 export function createContext(): Context {
 	return new Context();
+};
+
+export function createIncreasingOrder<A extends Value>(): IncreasingOrder<A> {
+	return new IncreasingOrder();
+};
+
+export function createDecreasingOrder<A extends Value>(): DecreasingOrder<A> {
+	return new DecreasingOrder();
+};
+
+export function createEqualityFilter<A extends Value>(value: A): EqualityFilter<A> {
+	return new EqualityFilter(value);
 };
