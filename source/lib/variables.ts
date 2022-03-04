98e245eb82fe750d62f23895508ae9a975b26b22
Joel Ek <contact@joelek.se>
2022-03-04 18:45:35 +0100

    Generalize parsing of environment variables.

--- source/lib/variables.ts
+++ source/lib/variables.ts
@@ -1,1 +1,12 @@
-export const DEBUG = (globalThis as any)?.process?.env["DEBUG"] === "true";
+export function getBoolean(key: string, defaultValue: boolean): boolean {
+	let string = (globalThis as any)?.process?.env[key];
+	try {
+		let json = JSON.parse(string);
+		if (typeof json === "boolean") {
+			return json;
+		}
+	} catch (error) {}
+	return defaultValue;
+};
+
+export const DEBUG = getBoolean("DEBUG", false);
