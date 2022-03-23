e9d6f630f6b15180b9f64381b5d2bd2965de4292
Joel Ek <contact@joelek.se>
2022-03-23 08:09:45 +0100

    Add support for filtering orphaned link records.

--- source/lib/links.ts
+++ source/lib/links.ts
@@ -4,7 +4,7 @@ import { Key, Keys, KeysRecord, KeysRecordMap, Record, RequiredKeys } from "./re
 import { Index, Store, StoreManager } from "./stores";
 
 export interface ReadableLink<A extends Record, B extends RequiredKeys<A>, C extends Record, D extends RequiredKeys<C>, E extends KeysRecordMap<A, B, C>> {
-	filter(keysRecord: KeysRecord<A, B>, anchor?: KeysRecord<C, D>): Promise<Iterable<C>>;
+	filter(keysRecord?: KeysRecord<A, B>, anchor?: KeysRecord<C, D>): Promise<Iterable<C>>;
 	lookup(record: C | Pick<C, E[B[number]]>): Promise<A | undefined>;
 };
 
@@ -73,12 +73,12 @@ export class LinkManager<A extends Record, B extends RequiredKeys<A>, C extends 
 		return this.child;
 	}
 
-	filter(keysRecord: KeysRecord<A, B>, anchor?: KeysRecord<C, D>): Iterable<C> {
+	filter(keysRecord?: KeysRecord<A, B>, anchor?: KeysRecord<C, D>): Iterable<C> {
 		let filters = {} as FilterMap<C>;
 		for (let key in this.keysRecordMap) {
 			let keyOne = key as any as B[number];
 			let keyTwo = this.keysRecordMap[keyOne];
-			filters[keyTwo] = new EqualityFilter(keysRecord[keyOne]) as any;
+			filters[keyTwo] = new EqualityFilter(keysRecord?.[keyOne] ?? null) as any;
 		}
 		return this.child.filter(filters, this.orders, anchor);
 	}
