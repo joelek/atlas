a27c244013f9342af7328616ef1a5a5c8ca8acdb
Joel Ek <contact@joelek.se>
2022-03-23 07:32:34 +0100

    Add type helpers.

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -1,7 +1,24 @@
-import { Context } from "./contexts";
+import { Context, LinkReference, QueryReference, StoreReference } from "./contexts";
 import { DecreasingOrder, IncreasingOrder } from "./orders";
 import { EqualityFilter } from "./filters";
 import { Value } from "./records";
+import { ReadableLink, WritableLink } from "./links";
+import { ReadableQuery, WritableQuery } from "./queries";
+import { ReadableStore, WritableStore } from "./stores";
+
+export type RecordOf<A> = A extends StoreReference<infer C, infer D> ? C : never;
+
+export type ReadableStoreOf<A> = A extends StoreReference<infer C, infer D> ? ReadableStore<C, D> : never;
+
+export type WritableStoreOf<A> = A extends StoreReference<infer C, infer D> ? WritableStore<C, D> : never;
+
+export type ReadableLinkOf<A> = A extends LinkReference<infer C, infer D, infer E, infer F, infer G> ? ReadableLink<C, D, E, F, G> : never;
+
+export type WritableLinkOf<A> = A extends LinkReference<infer C, infer D, infer E, infer F, infer G> ? WritableLink<C, D, E, F, G> : never;
+
+export type ReadableQueryOf<A> = A extends QueryReference<infer C, infer D, infer E, infer F> ? ReadableQuery<C, D, E, F> : never;
+
+export type WritableQueryOf<A> = A extends QueryReference<infer C, infer D, infer E, infer F> ? WritableQuery<C, D, E, F> : never;
 
 export function createContext(): Context {
 	return new Context();
