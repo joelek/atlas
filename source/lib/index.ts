bd88cbf88ca04f2bf9126366f61d5bc3f11d07e1
Joel Ek <contact@joelek.se>
2022-05-04 14:23:23 +0200

    Add createTransactionManager() to top level API.

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -1,8 +1,11 @@
-import { Context, StoreReference } from "./contexts";
+import { Context, LinkReferences, LinksFromLinkReferences, QueriesFromQueryReferences, QueryReferences, StoreReference, StoreReferences, StoresFromStoreReferences } from "./contexts";
 import { DecreasingOrder, IncreasingOrder } from "./orders";
 import { EqualityFilter } from "./filters";
 import { Value } from "./records";
 import * as transactions from "./transactions";
+import { WritableLinksFromLinkManagers, LinkManagersFromLinks } from "./links";
+import { WritableQueriesFromQueryManagers, QueryManagersFromQueries } from "./queries";
+import { WritableStoresFromStoreManagers, StoreManagersFromStores } from "./stores";
 
 export type ReadableQueue = transactions.ReadableQueue;
 
@@ -12,6 +15,12 @@ export type RecordOf<A> = A extends StoreReference<infer C, infer D> ? C : never
 
 export function createContext(): Context {
 	return new Context();
+};
+
+export function createTransactionManager<A extends StoreReferences<any>, B extends LinkReferences<any>, C extends QueryReferences<any>>(path: string, schemaProvider: (context: Context) => { stores?: A, links?: B, queries?: C }): transactions.TransactionManager<WritableStoresFromStoreManagers<StoreManagersFromStores<StoresFromStoreReferences<A>>>, WritableLinksFromLinkManagers<LinkManagersFromLinks<LinksFromLinkReferences<B>>>, WritableQueriesFromQueryManagers<QueryManagersFromQueries<QueriesFromQueryReferences<C>>>> {
+	let context = new Context();
+	let schema = schemaProvider(context);
+	return context.createTransactionManager(path, schema.stores, schema.links, schema.queries);
 };
 
 export function createIncreasingOrder<A extends Value>(): IncreasingOrder<A> {
