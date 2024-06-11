f3f49711e51555c112a36544bdd59818235ca7b6
Joel Ek <contact@joelek.se>
2024-06-11 22:43:27 +0200

    Add tests for flattening StreamIterables.

---
+++ source/lib/streams.test.ts
@@ -0,0 +1,16 @@
+import * as wtf from "@joelek/wtf";
+import { StreamIterable } from "./streams";
+
+wtf.test(`It should flatten Iterable<string> into Iterable<string>.`, async (assert) => {
+	let iterable = StreamIterable.of(["a", "b"]);
+	let observed = iterable.flatten().collect();
+	let expected = ["a", "b"];
+	assert.equals(observed, expected);
+});
+
+wtf.test(`It should flatten Iterable<Iterable<string>> into Iterable<string>.`, async (assert) => {
+	let iterable = StreamIterable.of(["a", "b"]).map((v) => StreamIterable.of(v === "a" ? ["a", "b"] : ["c", "d"]));
+	let observed = iterable.flatten().collect();
+	let expected = ["a", "b", "c", "d"];
+	assert.equals(observed, expected);
+});
